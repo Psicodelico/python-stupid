c2aa7bd8d04999b5be570bfa56d71de22d173a8d
这 <epoch@zhedeMacBook-Pro.local>
2019-11-28 14:43:06 +0800

    c26-01

--- test.py
+++ test.py
@@ -1,22 +1,3 @@
 
-def magic(listPrice, amount):
-    result = []
-    length = len(listPrice)
-    def calc(listNum, listPrice, amount):
-        n = 1
-        curPrice = listPrice.pop()
-        while(n*curPrice<amount & len(listPrice)>0):
-            if(length == len(listNum)):
-                tmp = 0
-                for i in range(length):
-                    tmp += listPrice[i]*listNum[i]
-                    if(tmp == amount):
-                        result.push(listNum)
-                        return
-            listNum.push(n)
-            calc(listNum, listPrice, amount)
-            n+=1
-    calc([], listPrice, amount)
-    return result
-
-print(magic([78, 36, 25, 178, 65, 18], 7500))
+from c25.ex import ex
+print(ex.break_words("asdfasf werr ryey hgn fgjrt"))
